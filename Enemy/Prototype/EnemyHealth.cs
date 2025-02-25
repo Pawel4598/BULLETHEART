a13a5633404d4f9abff3778f7e5d93cc92cc617c
Pawel4598 <pawelzybura7@gmail.com>
2025-02-25 13:45:37 +0000

    Update EnemyHealth.cs

--- Enemy/Prototype/EnemyHealth.cs
+++ Enemy/Prototype/EnemyHealth.cs
@@ -6,11 +6,15 @@ public class EnemyHealth : MonoBehaviour
 {
     public float maxHealth = 100f;
     public float currentHealth;
+    private SpriteRenderer spriteRenderer;
+    public Color flashColor = Color.red;
+    public float flashDuration = 0.1f;
 
     // Start is called before the first frame update
     void Start()
     {
-        currentHealth = maxHealth;  
+        currentHealth = maxHealth;
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -24,8 +28,18 @@ public class EnemyHealth : MonoBehaviour
         currentHealth -= damageAmount;
         Debug.Log(gameObject.name + " " + damageAmount + " " + currentHealth);
 
+        StartCoroutine(FlashRed());
+
         if (currentHealth <= 0)
             Die();
+    }
+
+    IEnumerator FlashRed()
+    {
+        Color originalColor = spriteRenderer.color;
+        spriteRenderer.color = flashColor;
+        yield return new WaitForSeconds(flashDuration);
+        spriteRenderer.color = originalColor;
     }
 
     void Die()
