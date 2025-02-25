5ceea7fc703c1e1b14b8eaedec2c8ea13064efc8
Pawel4598 <pawelzybura7@gmail.com>
2025-02-25 13:40:55 +0000

    Update PlayerHealth.cs

--- Player/PlayerHealth.cs
+++ Player/PlayerHealth.cs
@@ -2,16 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     public float maxHealth = 100f;
     public float currentHealth;
+    public HealthBar healthBar;
+    private SpriteRenderer spriteRenderer;
+    public Color flashColor = Color.red;
+    public float flashDuration = 0.1f;
+
 
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -25,8 +34,20 @@ public class PlayerHealth : MonoBehaviour
         currentHealth -= damageAmount;
         Debug.Log(gameObject.name + damageAmount + currentHealth);
 
+        StartCoroutine(FlashRed());
+
+        healthBar.SetHealth(currentHealth); 
+
         if (currentHealth <= 0)
             Die();
+    }
+
+    IEnumerator FlashRed()
+    {
+        Color originalColor = spriteRenderer.color;
+        spriteRenderer.color = flashColor;
+        yield return new WaitForSeconds(flashDuration);
+        spriteRenderer.color = originalColor;
     }
 
     void Die()
@@ -34,3 +55,4 @@ public class PlayerHealth : MonoBehaviour
         SceneManager.LoadScene("SampleScene", LoadSceneMode.Single); 
     }
 }
+
