76784d4699d01d3f87f6d7f5ce245b36c23033cc
Pawel4598 <pawelzybura7@gmail.com>
2025-03-13 13:03:47 +0000

    Update and rename Buttons.cs to PressurePlate.cs

--- Interaction/PressurePlate.cs
+++ Interaction/PressurePlate.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Buttons : MonoBehaviour
+public class PressurePlate : MonoBehaviour
 {
     public GameObject door;
     public float timer = 5f;
